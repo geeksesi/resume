3256a73e47d87f68beeede1566fad2c757a04027
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2023-01-27 09:54:00 +0330

    #5 feat: dynamic routing by multiple resume title

---
+++ gatsby-node.js
@@ -0,0 +1,21 @@
+exports.createPages = async ({ graphql, actions }) => {
+    const { data } = await graphql(`
+        query resumeQuery {
+            allFile(filter: { ext: { eq: ".json" } }) {
+                nodes {
+                    id
+                    name
+                }
+            }
+        }
+    `);
+    data.allFile.nodes.forEach((node) => {
+        actions.createPage({
+            path: node.name,
+            component: require.resolve("./src/templates/resume.js"),
+            context: {
+                slug: node.name,
+            },
+        });
+    });
+};
