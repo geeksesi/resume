019eb44bd085baf0b1704dfe12c637e56aac49e9
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2023-06-25 19:45:54 +0330

    #12 feat: ats

---
+++ src/templates/ats.js
@@ -0,0 +1,68 @@
+import * as React from "react";
+import PDFLayout from "../components/PDFLayout/index";
+import "../styles/index.css";
+import { graphql } from "gatsby";
+import { PDFViewer } from "@react-pdf/renderer";
+
+export default (data) => {
+    const [isClient, setIsClient] = React.useState(false);
+    React.useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    const resume = data.data.resumeJson;
+
+    return (
+        <div>
+            {isClient && (
+                <PDFViewer className="w-full h-screen">
+                    <PDFLayout resume={resume} />
+                </PDFViewer>
+            )}
+        </div>
+    );
+};
+
+export const Head = () => <title>Resume Mohammad Javad Ghasemy</title>;
+
+export const query = graphql`
+    query ($slug: String!) {
+        resumeJson(slug: { eq: $slug }) {
+            id
+            slug
+            name
+            title
+            contact {
+                link
+                name
+                title
+            }
+            social {
+                title
+                name
+                link
+            }
+            skills
+            projects {
+                title
+                url
+                thumbnail
+                stack
+            }
+            about
+            experiences {
+                title
+                role
+                company
+                time
+                type
+                url
+                description
+                responsibilities
+                technologies
+            }
+            languages
+            educations
+        }
+    }
+`;
